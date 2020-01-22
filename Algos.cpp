02c27610245e0451d8abc336838986a3e46224f6
Chris Brinton <cbrinton@gmail.com>
2020-01-22 14:49:42 -0500

    tutorial

---
+++ Algos.cpp
@@ -0,0 +1,10 @@
+#include "Algos.h"
+
+int MAC(int x, int y, int& sum) {
+   sum += x * y;
+   return sum;
+}
+
+int Square(int x) {
+   return x * x;
+}
---
+++ Algos.h
@@ -0,0 +1,7 @@
+#ifndef ALGOS_H
+#define ALGOS_H
+
+int MAC(int x, int y, int& sum);
+int Square(int x);
+
+#endif /* ALGOS_H */
